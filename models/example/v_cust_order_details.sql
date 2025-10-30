8527e30f73243f413a694bccfc0040b7c00e4aa1
Mayur Marke <mayur4710marke@gmail.com>
2025-10-30 17:55:30 +0000

    commit the changes of views

---
+++ models/example/v_cust_order_details.sql
@@ -0,0 +1,12 @@
+{{
+    config(
+        materialized='table',
+        transient=true
+    )
+}}
+
+SELECT first_name, o.status, o.order_date, paymentmethod, amount from customers c
+inner join orders o on o.customer_id=c.customer_id
+inner join payment p on o.order_id=p.orderid
+where c.status='Y'
+
