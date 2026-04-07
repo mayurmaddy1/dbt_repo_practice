2eabf086218a399ca6285d5bfb18f7cd1d059d04
Mayur Marke <markemayur1111@gmail.com>
2026-04-07 10:58:04 +0000

    commiting the sources file(rather than changing/hardcoding the name of DB and schema, it is better to create a source and then refer the source values

--- models/view_drivers_timings.sql
+++ models/view_drivers_timings.sql
@@ -9,7 +9,7 @@
 SELECT 
 p.raceid ,p.driverid,p.time, p.duration,
 d.driverref, d.code,d.nationality,d.number
-FROM PITSTOPS p
+FROM {{ source('s1', 'Circuits') }} p
 INNER JOIN
 DRIVERS d
 WHERE p.driverid =d.driverid
